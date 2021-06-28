695643906e2d71925b8e523755f278484bfbe85b
Zack Borges <zackartborges@gmail.com>
2021-06-28 08:16:20 -0400

    adds index get method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import axios from "axios";
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 
@@ -14,13 +15,23 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      stateData: [],
     };
   },
   name: "Home",
   components: {
     // HelloWorld,
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexStates();
+  },
+  methods: {
+    indexStates: function () {
+      axios.get("api/states").then((response) => {
+        this.states = response.data;
+        console.log("all state data:", this.states);
+      });
+    },
+  },
 };
 </script>
