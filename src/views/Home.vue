d5505ef469c25861d51c9b9342abb0f9c4e32f19
Zack Borges <zackartborges@gmail.com>
2021-06-27 23:10:26 -0400

    sets up home page , adds methods and data points

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h1>Hey !</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
+  created: function () {},
+  methods: {},
 };
 </script>
