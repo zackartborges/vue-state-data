22a0c15132aab701e3a8367f3358d43e838e2c7d
Zack Borges <zackartborges@gmail.com>
2021-07-24 18:48:26 -0400

    gets data on map

--- src/components/StateMap.vue
+++ src/components/StateMap.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="state-map">
+  <div class="body">
     <h1>Map Of The United States</h1>
-    <div id="map_election" style="position: relative; width: 500px; height: 300px"></div>
+    <div id="map_election" style="position: relative; width: 800px; height: 500px"></div>
   </div>
 </template>
 
@@ -18,6 +18,7 @@ export default {
   data: function () {
     return {
       states: [],
+      // reformattedData: {},
     };
   },
   name: "StateMap",
@@ -32,6 +33,16 @@ export default {
     getStateData: function () {
       axios.get("api/states").then((response) => {
         this.states = response.data;
+        let reformattedData = {};
+        this.states.forEach((object) => {
+          reformattedData[object["state_code"]] = {
+            medianHouseholdIncome: object["median_household_income"],
+            sharePopulationInMetroAreas: object["share_population_in_metro_areas"],
+            sharePopulationWithHighSchoolDegree: object["share_population_with_high_school_degree"],
+            shareUnemployedSeasonal: object["share_unemployed_seasonal"],
+            stateAbbrev: object["state_abbrev"],
+          };
+        });
         // var mappedStates = this.states.forEach(function(state) {
         //   state.state_code
         // });
@@ -41,222 +52,36 @@ export default {
           geographyConfig: {
             highlightBorderColor: "#bada55",
             popupTemplate: function (geography, data) {
-              return '<div class="hoverinfo">' + "State data:" + data.electoralVotes + " ";
+              return (
+                '<div class="hoverinfo">' +
+                geography.properties.name +
+                "<br>" +
+                " Median Household Income: " +
+                data.medianHouseholdIncome +
+                // "<br>" +
+                " Share Population in Metro Areas: " +
+                data.sharePopulationInMetroAreas +
+                " "
+              );
             },
             highlightBorderWidth: 3,
           },
           fills: {
             defaultFill: "#EDDC4E",
           },
-          // map through this.states: data: this.states.map
-
-          data: {
-            AZ: {
-              fillKey: "Republican",
-              electoralVotes: response.data[0]["median_household_income"],
-            },
-            CO: {
-              fillKey: "Light Democrat",
-              electoralVotes: 5,
-            },
-            DE: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            FL: {
-              fillKey: "UNDECIDED",
-              electoralVotes: 29,
-            },
-            GA: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            HI: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            ID: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            IL: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            IN: {
-              fillKey: "Republican",
-              electoralVotes: 11,
-            },
-            IA: {
-              fillKey: "Light Democrat",
-              electoralVotes: 11,
-            },
-            KS: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            KY: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            LA: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            MD: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            ME: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            MA: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            MN: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            MI: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            MS: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            MO: {
-              fillKey: "Republican",
-              electoralVotes: 13,
-            },
-            MT: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            NC: {
-              fillKey: "Light Republican",
-              electoralVotes: 32,
-            },
-            NE: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            NV: {
-              fillKey: "Heavy Democrat",
-              electoralVotes: 32,
-            },
-            NH: {
-              fillKey: "Light Democrat",
-              electoralVotes: 32,
-            },
-            NJ: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            NY: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            ND: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            NM: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            OH: {
-              fillKey: "UNDECIDED",
-              electoralVotes: 32,
-            },
-            OK: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            OR: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            PA: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            RI: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            SC: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            SD: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            TN: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            TX: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            UT: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            WI: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            VA: {
-              fillKey: "Light Democrat",
-              electoralVotes: 32,
-            },
-            VT: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            WA: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            WV: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            WY: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            CA: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            CT: {
-              fillKey: "Democrat",
-              electoralVotes: 32,
-            },
-            AK: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            AR: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-            AL: {
-              fillKey: "Republican",
-              electoralVotes: 32,
-            },
-          },
+          data: reformattedData,
         });
         election.labels();
         console.log("all state data:", this.states);
+        console.log("reformatted data:", reformattedData);
       });
     },
   },
 };
 </script>
+
+<style>
+html {
+  background-color: darkkhaki;
+}
+</style>
